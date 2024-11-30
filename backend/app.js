97a4a5cd9396f4bfe13a42c5c83f4d6f3c5d11a7
Elias Soykat <eliasmd624@gmail.com>
2024-11-30 17:47:51 +0600

    Fix : update environment variable

--- backend/app.js
+++ backend/app.js
@@ -31,9 +31,9 @@ app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
 // Connect frontend to backend
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
 
 // Errors Middleware
